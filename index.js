952c80ac7ba0e4328bc6d6fcf844dd8858e45a78
V4l33eria <rubiovaleria123@gmail.com>
2025-05-21 15:23:35 -0600

    initial_commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/temperatura", (req, res) => {
+  res.json({ valor: "10°C", timestamp: new Date().toISOString() });
+});
+
+
+app.get("/otro", (req, res) => {
+  res.json({ valor: "Hola", timestamp: new Date().toISOString() });
+});
+
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en puerto ${port}`);
+});
