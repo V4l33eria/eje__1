e1736829a851bfe7d597390a25f2b70f8e7804e6
V4l33eria <rubiovaleria123@gmail.com>
2025-08-20 18:29:09 -0600

    tokennn

--- index.js
+++ index.js
@@ -193,7 +193,7 @@ app.post("/turn-off", verifyToken, async (req, res) => {
 app.get("/logs", async (req, res) => {
   try {
     const token = req.headers.authorization?.split(" ")[1];
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, SECRET_KEY);
     const userEmail = decoded.email;
 
     const result = await pool.query(
